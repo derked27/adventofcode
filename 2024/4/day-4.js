668bf3c617a86d5f4d1ef5180c4ee145a01ea5f1
derked27 <derked27@gmail.com>
2024-12-04 19:58:28 -0700

    Part 1 complete

--- 2024/4/day-4.js
+++ 2024/4/day-4.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const Node = require('./node');
+const { Node } = require('./node');
+const { DIRECTION } = require('./direction.enum');
 
 async function main(params) {
-    const filePath = path.join(__dirname, '../input/day-3');
+    const filePath = path.join(__dirname, '../input/day-4');
     const fileStream = fs.createReadStream(filePath);
     const readLine = readline.createInterface({ input: fileStream, });
+    const searchString = "XMAS";
     let y = 0;
     let nodeMap = {};
     let startNodes = [];
@@ -14,16 +16,71 @@ async function main(params) {
         let characterList = line.split('');
         for (let x = 0; x < characterList.length; x++) {
             const char = characterList[x];
-            let mapKey = x+'|'+y;
-            if (!nodeMap[mapKey]) {
-                let node = new  Node(x, y, char);
-            }
-
-            
+            let mapKey = getMapKey(x, y);
+            let node = new Node(x, y, char);
+            nodeMap[mapKey] = node;
+            if (char == 'X')
+                startNodes.push(node);
         }
         y++;
     }
+
+    Object.values(nodeMap).forEach(node => {
+        populateEdges(node, nodeMap);
+    });
+
+    let result = 0;
+    for (const node of startNodes) {
+        for(const direction of Object.values(DIRECTION)) {
+            if (node.traverse(direction, searchString, 0)) {
+                result++;
+            }
+        }
+    }
+
+    console.log(result);
+
+    console.log(nodeMap);
+}
+
+function getMapKey(x, y) {
+    return x + '|' + y;
+}
+
+function populateEdges(node, nodeMap) {
+    for (let x = node.x - 1; x < node.x + 2; x++) {
+        for (let y = node.y - 1; y < node.y + 2; y++) {
+            const edge = nodeMap[getMapKey(x,y)];
+            if (edge) {
+                const direction = findDirection(node.x, node.y, x, y)
+                node.setEdge(direction, edge);
+            }
+        }
+    }
+
+    //   0 0    1 0    2 0
+    //   0 1    1 1    2 1
+    //   0 2    1 2    2 2
+}
+
+function findDirection(nodeX, nodeY, foundX, foundY) {
+    if (foundX < nodeX && foundY < nodeY)
+        return DIRECTION.TOPLEFT;
+    if (foundX == nodeX && foundY < nodeY)
+        return DIRECTION.TOP;
+    if (foundX > nodeX && foundY < nodeY)
+        return DIRECTION.TOPRIGHT;
+    if (foundX < nodeX && foundY == nodeY)
+        return DIRECTION.LEFT;
+    if (foundX > nodeX && foundY == nodeY)
+        return DIRECTION.RIGHT;
+    if (foundX < nodeX && foundY > nodeY)
+        return DIRECTION.BOTTOMLEFT;
+    if (foundX == nodeX && foundY > nodeY)
+        return DIRECTION.BOTTOM;
+    if (foundX > nodeX && foundY > nodeY)
+        return DIRECTION.BOTTOMRIGHT;
 }
 
 
-module.exports = { evalString, main }
+module.exports = { main }
--- 2024/4/node.js
+++ 2024/4/node.js
@@ -1,6 +1,6 @@
 const DIRECTION = require('./direction.enum');
 
-export class Node {
+class Node {
     constructor(x, y, value) {
         this.x = x;
         this.y = y;
@@ -11,4 +11,25 @@ export class Node {
     setEdge(direction, node) {
         this.edges[direction] = node;
     }
+
+    traverse(direction, searchString, depth) {
+        // At end
+        if (searchString.length - 1 == depth && searchString[searchString.length - 1] == this.value)
+            return true;
+
+        // If no value at next direction
+        if (!this.edges[direction])
+            return false;
+
+        // Determine if current node is valid
+        if (searchString[depth] != this.value)
+            return false
+
+        return this.edges[direction].traverse(direction, searchString, ++depth);
+    }
+
+
+
 }
+
+module.exports = { Node }
